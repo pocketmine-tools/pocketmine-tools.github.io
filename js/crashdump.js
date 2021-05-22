2c924b9a2cab7c46f5d9310ddaa4697a52ff6b4d
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-05-22 11:40:21 -0700

    Update crashdump.js

--- js/crashdump.js
+++ js/crashdump.js
@@ -5,21 +5,17 @@ document.getElementById('exampleForm.ControlTextarea1').onchange = function() {
     crashdump = crashdump.replace("===BEGIN CRASH DUMP===", "");
     crashdump = crashdump.replace("===END CRASH DUMP===", "");
 
-    try {
-        const decodedBase64 = atob(crashdump);
-        // var bytes = [];
-        // for (var i = 0; i < decodedBase64.length; ++i) {
-        //     var code = decodedBase64.charCodeAt(i);
-        //     bytes = bytes.concat([code & 0xff, code / 256 >>> 0]);
-        // }
-        // const inflatedZlib = pako.inflate(bytes);
-        const inflatedZlib = pako.inflate(new Uint8Array(decodedBase64), {"to":"string"});
-        const decodedCrashdump = new encoding.TextDecoder('utf-8').decode(inflatedZlib);
-        let jsonData = JSON.parse(decodedCrashdump);
-        jsonData = JSON.stringify(jsonData, null, 2);
-    } catch (error) {
-        console.error(error);
-    }
+    const decodedBase64 = atob(crashdump);
+    // var bytes = [];
+    // for (var i = 0; i < decodedBase64.length; ++i) {
+    //     var code = decodedBase64.charCodeAt(i);
+    //     bytes = bytes.concat([code & 0xff, code / 256 >>> 0]);
+    // }
+    // const inflatedZlib = pako.inflate(bytes);
+    const inflatedZlib = pako.inflate(new Uint8Array(decodedBase64), {"to":"string"});
+    const decodedCrashdump = new encoding.TextDecoder('utf-8').decode(inflatedZlib);
+    let jsonData = JSON.parse(decodedCrashdump);
+    jsonData = JSON.stringify(jsonData, null, 2);
 
     if (jsonData.length >= 1) {
         var label = document.createElement("Label");
